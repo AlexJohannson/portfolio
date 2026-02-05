58c90c4fcbde2622d48fcdec4ac94ef44fdb5796
AlexJohannson <mehedsanya@gmail.com>
2026-02-05 11:51:29 +0100

    Initial commit

--- src/components/AboutMeComponent/AboutMeComponent.tsx
+++ src/components/AboutMeComponent/AboutMeComponent.tsx
@@ -4,19 +4,6 @@ const AboutMeComponent = () => {
     return (
         <div className='about-me-container'>
             <img src="/img/my-photo/oleksandr.png" className='about-me-img' alt="Oleksandr" />
-            {/*<div className='about-me-text'>*/}
-            {/*    <h1>Hello World! I am Oleksandr Mekhed</h1>*/}
-            {/*    <h2>Fullstack Developer</h2>*/}
-            {/*    <p>*/}
-            {/*        Fullstack developer with a backend-first mindset and strong frontend execution. I build*/}
-            {/*        production-grade platforms using Django, Django REST Framework, Python, Celery, Redis, WebSocket,*/}
-            {/*        React, Next.js, Redux, and TypeScript. I’ve delivered real-time systems with tests, role-based access,*/}
-            {/*        and responsive UIs. Skilled in bridging business logic and user experience through clean architecture*/}
-            {/*        and asynchronous workflows. Focused on developing reliable digital solutions and ready*/}
-            {/*        to apply skills in professional teams.*/}
-            {/*    </p>*/}
-
-            {/*</div>*/}
             <div className='about-me-text'>
                 <h1>Hello World! I am Oleksandr Mekhed</h1>
                 <h2>Junior Fullstack Developer</h2>
